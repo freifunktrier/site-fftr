fdeb9fb6fe751bcd018d120859eb243f800af0d8
dracoTrier <github@basisbit.de>
2015-02-12 22:54:43 +0100

    Fidepus sign key, minor tweaks
    added fidepus' sign key, changed tabs, changed peer-registration-address
    to trier@freifunk.net (maybe use an admin-mailinglist later), lowered
    the mesh_mcast rate for 2.4 GHz (change this back to 12 when we have
    more router density in City)

--- site.mk
+++ site.mk
@@ -20,9 +20,9 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.6.2+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.6.3+exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
-GLUON_PRIORITY ?= 10
+GLUON_PRIORITY ?= 7
