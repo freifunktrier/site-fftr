31728b30b665b41575cda9e180d6534e7a39d059
dracoTrier <github@basisbit.de>
2015-05-17 15:40:01 +0200

    added new modules, multilanguage support, fix fastd
    added modules luci-wifi-config & luci-mesh-vpn-fastd and multilanguage
    support; also fixed fastd configuration for gluon v2015.1

--- site.mk
+++ site.mk
@@ -8,24 +8,30 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
+	gluon-luci-mesh-vpn-fastd \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
+	gluon-setup-mode \
 	gluon-status-page \
 	iwinfo \
 	iptables \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.6.6+beta$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.6.7+beta$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_PRIORITY ?= 1
+
+GLUON_LANGS ?= en de
