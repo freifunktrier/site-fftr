a2bdaa6d30202bdfef6b26a74bc1015b12a0428e
tackin <tackin@mail2.digital-layout.de>
2017-07-02 15:01:44 +0200

    try somthing ele

--- check/validate_site.sh
+++ check/validate_site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# validate_site.sh checks if the site.conf is valid json
+# validate_site.sh checks if the site.conf is valid lua.dict
 
 GLUON_SITEDIR="." lua5.1 check/site_config.lua
