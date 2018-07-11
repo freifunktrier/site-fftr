f3ff3d7ce18c2c02465dbfa4fa217f82967ced09
tackin <tackin@mail2.digital-layout.de>
2018-07-11 08:39:21 +0200

    prepare for new gluon 2018.1 build-rules

--- site.mk
+++ site.mk
@@ -1,34 +1,38 @@
-GLUON_SITE_PACKAGES := \
-	gluon-alfred \
-	gluon-respondd \
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-web-admin \
-	gluon-web-autoupdater \
-	gluon-web-network \
-	gluon-web-wifi-config \
-	gluon-web-private-wifi \
-	gluon-mesh-batman-adv-15 \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
-	haveged \
-iwinfo
+##	GLUON_FEATURES
+#		Specify Gluon features/packages to enable;
+#		Gluon will automatically enable a set of packages
+#		depending on the combination of features listed
+
+GLUON_FEATURES := \
+	autoupdater \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	ebtables-limit-arp \
+	mesh-batman-adv-15 \
+	mesh-vpn-fastd \
+	radvd \
+	respondd \
+	mesh-batman-adv-15 \
+	status-page \
+	web-advanced \
+	web-private-wifi \
+	web-wizard
+
+##	GLUON_SITE_PACKAGES
+#		Specify additional Gluon/LEDE packages to include here;
+#		A minus sign may be prepended to remove a packages from the
+#		selection that would be enabled by default or due to the
+#		chosen feature flags
+
+GLUON_SITE_PACKAGES := haveged iwinfo
+
 	#disabled because it's broken anyway and does only confuse people if present but broken
+        # gluon-alfred \
 	# gluon-config-mode-supernode \
-	
 	# not sure but think it's obsolete
 	# gluon-fastd-tunneling-mtu-workaround \
 
-DEFAULT_GLUON_RELEASE := 0.10.$(shell date '+%Y%m%d')_exp
+DEFAULT_GLUON_RELEASE := 0.11.$(shell date '+%Y%m%d')_exp
 # DEFAULT_GLUON_RELEASE := 0.9.1
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
