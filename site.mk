967c70bf544f90490b7656c19f3e410b72347e74
tackin <tackin@mail2.digital-layout.de>
2019-03-22 14:42:00 +0100

    Prepare our gluon 2018.2.1 for domain-switch to 802.11s
    First suggestions for some changes.

--- site.mk
+++ site.mk
@@ -12,18 +12,21 @@ GLUON_FEATURES := \
 	mesh-vpn-fastd \
 	radvd \
 	respondd \
-	mesh-batman-adv-15 \
 	status-page \
 	web-advanced \
 	web-private-wifi \
 	web-wizard
 
+##	GLUON_MULTIDOMAIN
+#		Build gluon with multidomain support.
+GLUON_MULTIDOMAIN=1
+
+	
 ##	GLUON_SITE_PACKAGES
 #		Specify additional Gluon/LEDE packages to include here;
 #		A minus sign may be prepended to remove a packages from the
 #		selection that would be enabled by default or due to the
 #		chosen feature flags
-
 GLUON_SITE_PACKAGES := haveged iwinfo
 
 	#disabled because it's broken anyway and does only confuse people if present but broken
@@ -32,8 +35,10 @@ GLUON_SITE_PACKAGES := haveged iwinfo
 	# not sure but think it's obsolete
 	# gluon-fastd-tunneling-mtu-workaround \
 
-DEFAULT_GLUON_RELEASE := 0.11.$(shell date '+%Y%m%d')_exp
-# DEFAULT_GLUON_RELEASE := 0.9.1
+
+
+DEFAULT_GLUON_RELEASE := 0.11.+exp$(shell date '+%Y%m%d')
+# DEFAULT_GLUON_RELEASE := 0.11.1
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
