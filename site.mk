cf911128fadd052fc8f3ba0a0f6163343f62ab2c
tackin <tackin@mail2.digital-layout.de>
2024-01-06 16:31:05 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -3,31 +3,6 @@
 #		Gluon will automatically enable a set of packages
 #		depending on the combination of features listed
 
-
-features({
-    'autoupdater',
-	'config-mode-outdoor',
-	'ebtables-filter-multicast',
-	'ebtables-filter-ra-dhcp',
-    'mesh-batman-adv-15',
-    'mesh-vpn-fastd-l2tp',
-    'respondd',
-    'status-page',
-    'web-advanced',
-	'web-private-wifi',
-    'web-wizard',
-})
-
-if not device_class('tiny') then
-    features({
-        'wireless-encryption-wpa3',
-    })
-end
-
-
-packages({'iwinfo'})
-
-
 # Build both sysupgrade and factory images for deprecated devices.
 GLUON_DEPRECATED=full
 
