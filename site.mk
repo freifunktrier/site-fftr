47d66d8d41149650c6e85762c301e1753b9eddd9
tackin <tackin@mail2.digital-layout.de>
2024-01-06 16:12:37 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -3,23 +3,30 @@
 #		Gluon will automatically enable a set of packages
 #		depending on the combination of features listed
 
-GLUON_FEATURES := \
-	autoupdater \
-	config-mode-outdoor \
-	ebtables-filter-multicast \
-	ebtables-filter-ra-dhcp \
-	mesh-batman-adv-15 \
-	mesh-vpn-fastd-l2tp \
-	respondd \
-	status-page \
-	web-advanced \
-	web-private-wifi \
-	web-wizard
-	
-# 	mesh-vpn-fastd \
-#	web-mesh-vpn-fastd \  # enable Web-optionsmenue for fastd-methodes in configmode
-#	ebtables-limit-arp \  # enabled by default since 2018.2.1
-# 	scheduled-domain-switch \
+
+features({
+    'autoupdater',
+	'config-mode-outdoor',
+	'ebtables-filter-multicast',
+	'ebtables-filter-ra-dhcp',
+    'mesh-batman-adv-15',
+    'mesh-vpn-fastd-l2tp',
+    'respondd',
+    'status-page',
+    'web-advanced',
+	'web-private-wifi',
+    'web-wizard',
+})
+
+if not device_class('tiny') then
+    features({
+        'wireless-encryption-wpa3',
+    })
+end
+
+
+packages({'iwinfo'})
+
 
 # Build both sysupgrade and factory images for deprecated devices.
 GLUON_DEPRECATED=full
@@ -28,29 +35,8 @@ GLUON_DEPRECATED=full
 #		Build gluon with multidomain support.
 GLUON_MULTIDOMAIN=1
 
-GLUON_FEATURES_standard := wireless-encryption-wpa3
 
-	
-##	GLUON_SITE_PACKAGES
-#		Specify additional Gluon/LEDE packages to include here;
-#		A minus sign may be prepended to remove a packages from the
-#		selection that would be enabled by default or due to the
-#		chosen feature flags
-#
-# GLUON_SITE_PACKAGES := haveged iwinfo
-#  removed in gluon V2020.2.x: haveged from  GLUON_SITE_PACKAGES, as urngd is now included by default.
-GLUON_SITE_PACKAGES := iwinfo
-
-
-	#disabled because it's broken anyway and does only confuse people if present but broken
-	# gluon-alfred \
-	# gluon-config-mode-supernode \
-	# not sure but think it's obsolete
-	# gluon-fastd-tunneling-mtu-workaround \
-
-
-
-DEFAULT_GLUON_RELEASE := 0.16.0.$(shell date '+%Y%m%d')+tackin
+DEFAULT_GLUON_RELEASE := 0.16.6.$(shell date '+%Y%m%d')+tackin
 # Just to name my testing-environments
 
 # Allow overriding the release number from the command line
